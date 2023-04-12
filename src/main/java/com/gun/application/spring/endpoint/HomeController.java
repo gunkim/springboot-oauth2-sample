ae2ea97d77190d3da307d45d8ed88e44c634cf36
gunkim <gunkim.dev@gmail.com>
2023-04-12 23:50:41 +0900

    :fire: 불필요 임포트 제거

--- src/main/java/com/gun/application/spring/endpoint/HomeController.java
+++ src/main/java/com/gun/application/spring/endpoint/HomeController.java
@@ -1,6 +1,5 @@
 package com.gun.application.spring.endpoint;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
