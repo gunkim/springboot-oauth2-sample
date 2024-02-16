244bb35180edd770107be48c2f22ca190a19c3f9
gunkim <gunkim.dev@gmail.com>
2024-02-17 02:37:08 +0900

    :recycle: jdk 21, spring security 6 이상으로 재세팅

--- src/main/java/io/github/gunkim/application/spring/security/SecurityConfig.java
+++ src/main/java/io/github/gunkim/application/spring/security/SecurityConfig.java
@@ -7,6 +7,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.client.OAuth2LoginConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -16,16 +20,21 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .headers().frameOptions().disable().and()
-                .authorizeHttpRequests()
-                .requestMatchers(HttpMethod.GET, "/", "/css/**", "/img/**", "/js/**", "/h2-console/**").permitAll()
-                .requestMatchers(HttpMethod.GET, "/user").hasRole(Role.USER.name())
-                .anyRequest().authenticated().and()
-                .logout().logoutSuccessUrl("/").and()
-                .oauth2Login().userInfoEndpoint()
-                .userService(customOAuth2UserService);
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .headers(headersConfigurer -> headersConfigurer.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+                .authorizeHttpRequests(this::configureHttpPermissions)
+                .logout(logoutConfigurer -> logoutConfigurer.logoutSuccessUrl("/"))
+                .oauth2Login(this::configureOAuth2Login)
+                .build();
+    }
 
-        return http.build();
+    private void configureHttpPermissions(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry requestMatcherRegistry) {
+        requestMatcherRegistry.requestMatchers(HttpMethod.GET, "/", "/css/**", "/img/**", "/js/**", "/h2-console/**").permitAll();
+        requestMatcherRegistry.requestMatchers(HttpMethod.GET, "/user").hasRole(Role.USER.name());
+        requestMatcherRegistry.anyRequest().authenticated();
+    }
+
+    private void configureOAuth2Login(OAuth2LoginConfigurer<HttpSecurity> oauth2LoginConfigurer) {
+        oauth2LoginConfigurer.userInfoEndpoint(endpointCustomizer -> endpointCustomizer.userService(customOAuth2UserService));
     }
 }
